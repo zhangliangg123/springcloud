9509d32efb4945c2d5dfd54d4e7b05aac9fe03e4
zhangliangg123 <2435019749@qq.com>
2019-02-24 22:31:45 +0800

    Ribbon负载均衡前提搭建

--- microservicecloud-provider-dept-8002/src/main/java/com/atguigu/springcloud/DeptProvider8002_App.java
+++ microservicecloud-provider-dept-8002/src/main/java/com/atguigu/springcloud/DeptProvider8002_App.java
@@ -2,13 +2,15 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class DeptConsumer80_App {
+@EnableDiscoveryClient//服务发现
+public class DeptProvider8002_App {
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(DeptConsumer80_App.class, args);
+		SpringApplication.run(DeptProvider8002_App.class, args);
 	}
 
 }
